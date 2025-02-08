496e65aa5caaf41932578bc9fdd47af1f3095325
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-02-08 19:50:46 +0200

    Learning about Classes and Objects. Objects seem to be the beast at the moment but I will conc... How do you spell concure?

--- Practice.py
+++ Practice.py
@@ -17,11 +17,27 @@ enclosing_func()"""
 # 08 February 2025
  
 
-x = [1,2,3]
+"""x = [1,2,3]
 
 def local():
     x[1] = 42
 
 print(x)
 local() #This now changes the x variable in the global scope and can be seen when printing x below.
-print(x) 
+print(x)"""
+
+#Classes and Objects
+
+#defining a class called Bike
+
+class Bike:
+
+    def __init__(self, colour, frame_material):
+        self.colour = colour
+        self.frame_material = frame_material
+
+    def brake(self):
+        print("Braking")
+
+red_bike = Bike ("Red", "Carbon fiber")
+blue_bike = Bike("Blue", "Steel")    
