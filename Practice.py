71eba8f8af4ef1e8b94f0c6a17e1972a2c6b522e
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-02-08 19:18:49 +0200

    Scoping never seazes to amaze me.

--- Practice.py
+++ Practice.py
@@ -1,6 +1,6 @@
 #Scopes
 
-def enclosing_func ():
+"""def enclosing_func ():
     m = 13
 
     def local():
@@ -11,4 +11,17 @@ def enclosing_func ():
 m = 5
 print(m)
 
-enclosing_func()
+enclosing_func()"""
+
+#Saturday [19:10]
+# 08 February 2025
+ 
+
+x = [1,2,3]
+
+def local():
+    x[1] = 42
+
+print(x)
+local() #This now changes the x variable in the global scope and can be seen when printing x below
+print(x) 
