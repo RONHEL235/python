8bd7fef30e075ca381ce3245e2498002cce77d97
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-02-14 21:09:48 +0200

    I have just written a line of code without looking at the textbook, cool cool

--- Practice.py
+++ Practice.py
@@ -223,9 +223,13 @@ print(a_string % "Fabrizio")
 #Example 2: Using the string as a template
 #You have a string that can be set as default and in that string can be spaces that can be left for modification
 
-the_first_string = "The name %s."
+"""the_first_string = "The name %s."
 the_second_string = "is so %s."
 
 the_final_string = the_first_string % "Ronee",the_second_string % "Unique"
 
-print(the_final_string)
+print(the_final_string)"""
+
+the_new_string = "The new string {} {}"
+the_new_new_string = the_new_string.format("is", "good")
+print(the_new_new_string)
