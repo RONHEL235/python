04e1a9dc0e00f629a164773383bea7b10ec54085
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-02-20 09:32:15 +0200

    The second while loop, a bit challenging

--- Practice.py
+++ Practice.py
@@ -292,3 +292,15 @@ while n > 0:
  remainders.insert(0, remainder) # we keep track of remainders
  n //= 2 # we divide n by 2
 print(remainders)
+
+
+#Second example
+# multiple.sequences.while.py
+people = ['Conrad', 'Deepak', 'Heinrich', 'Tom']
+ages = [29, 30, 34, 36]
+position = 0
+while position < len(people):
+ person = people[position]
+ age = ages[position]
+ print(person, age)
+ position += 1
