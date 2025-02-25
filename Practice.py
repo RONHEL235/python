07d5611700533119edbae7a2751ce96eb1ea7405
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-02-25 21:24:03 +0200

    I sleep.

--- Practice.py
+++ Practice.py
@@ -503,4 +503,20 @@ quadratic(1, 0, 1)"""
 #try
 #except
 #else
-#finally   
+#finally  
+ 
+def try_syntax(numerator, denominator):
+    try: 
+        print(f'In the try block: {numerator} {denominator}')
+        result = numerator / denominator
+    
+    except ZeroDivisionError as zde:
+        print(zde)
+    
+    else:
+        print('The result is:', result)
+        return result
+    finally:
+        print('Exiting')
+print(try_syntax(12, 4))
+print(try_syntax(11, 0))
