334a258e5e74dc806221caf48e143fbe9deab9a6
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-04 23:33:10 +0200

    Creating the class with no methods and variables

--- Practice.py
+++ Practice.py
@@ -859,11 +859,13 @@ def guess_the_number():
 #Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
 
 class Vehicle:
-    def __init__(self, max_speed, mileage):
+    def __init__(self, max_speed, mileage): #Methods
         self.max_speed = max_speed
         self.mileage = mileage
 
 modelX = Vehicle(240, 18)
 print(modelX.max_speed, modelX.mileage)
 
-    
+#OOP Exercise 2: Create a Vehicle class without any variables and methods
+class Vehicle:
+        pass
