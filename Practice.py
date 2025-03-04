7e945f3351f091bf5df3d25078fd8935d7b98464
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-04 23:37:05 +0200

    Created a Bus class that inherits everything from the Vehicle class

--- Practice.py
+++ Practice.py
@@ -869,3 +869,12 @@ print(modelX.max_speed, modelX.mileage)
 #OOP Exercise 2: Create a Vehicle class without any variables and methods
 class Vehicle:
         pass
+
+
+#OOP Exercise 3: Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+
+class Bus (Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
