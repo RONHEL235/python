6e9faf15f21a49a80df1d60b198b89e0900529bb
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-04 23:30:32 +0200

    That is how you define a class and call the class too.

--- Practice.py
+++ Practice.py
@@ -854,4 +854,16 @@ def guess_the_number():
     lower = int(input("Input the lower bound: "))
     upper = int(input("Input the upper bound: "))
 
+
+#OOP Exercise 1: Create a Class with instance attributes
+#Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+modelX = Vehicle(240, 18)
+print(modelX.max_speed, modelX.mileage)
+
     
