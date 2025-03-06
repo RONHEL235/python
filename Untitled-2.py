7b44b06e1d862ff30dc6d9b90d6df160674ec9c1
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-06 09:30:17 +0200

    Learning with importing os

--- Untitled-2.py
+++ Untitled-2.py
@@ -29,7 +29,23 @@ text = "Hello, World!   "
 clean_text = text.lstrip()
 print(f"'{clean_text}'")  # Output: 'Hello, World!'
 
-"   Hello World!  "
+#Working with File and Directories 
 
-#result
-"Hello World   "
+# files/walking.py
+import os
+for root, dirs, files in os.walk('.'):
+    abs_root = os.path.abspath(root)
+    print(abs_root)
+    
+    if dirs:
+        print('Directories:')
+
+    for dir_ in dirs:
+      print(dir_)
+
+    print()
+    if files:
+        print('Files:')
+    for filename in files:
+        print(filename)
+    print()
