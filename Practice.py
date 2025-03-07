aaaa1433ef040211bce0a30306214bc22fa4b227
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-07 10:36:15 +0200

    The actual solution. String formating

--- Practice.py
+++ Practice.py
@@ -981,3 +981,11 @@ quantity = 3
 price = 450
 
 print(f'I have {totalMoney} dollars so that I can buy {quantity} football for {price} dollars')
+
+#The actual solution 
+
+quantity = 3
+totalMoney = 1000
+price = 450
+statement1 = "I have {1} dollars so I can buy {0} football for {2:.2f} dollars."
+print(statement1.format(quantity, totalMoney, price))
