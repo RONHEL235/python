a7a6337272787f7069a23cf7aa23fc622ce26fac
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-08 20:25:21 +0200

    Nested loops

--- Practice.py
+++ Practice.py
@@ -1006,3 +1006,21 @@ i = 1
 while i <= 10:
     print(i)
     i += 1
+
+#Exercise 2: Print the following pattern
+#Write a Python code to print the following number pattern using a loop.
+
+print("Number Pattern ")
+
+# Decide the row count. (above pattern contains 5 rows)
+row = 5
+# start: 1
+# stop: row+1 (range never include stop number in result)
+# step: 1
+# run loop 5 times
+for i in range(1, row + 1, 1):
+    # Run inner loop i+1 times
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    # empty line after each row
+    print("")
