e70b98cc3a6fa85dea0b88d7d2ddf76e27575d4b
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-08 20:28:33 +0200

    Calculate sum of all numbers from 1 to a given number

--- Practice.py
+++ Practice.py
@@ -1024,3 +1024,19 @@ for i in range(1, row + 1, 1):
         print(j, end=' ')
     # empty line after each row
     print("")
+
+#Exercise 3: Calculate sum of all numbers from 1 to a given number
+#Write a Python program to accept a number from a user and calculate the sum of all numbers from 1 to a given number
+
+#For example, if the user entered 10, the output should be 55 (1+2+3+4+5+6+7+8+9+10)
+
+# s: store sum of all numbers
+s = 0
+n = int(input("Enter number "))
+# run loop n times
+# stop: n+1 (because range never include stop number in result)
+for i in range(1, n + 1, 1):
+    # add current number to sum variable
+    s += i
+print("\n")
+print("Sum is: ", s)
