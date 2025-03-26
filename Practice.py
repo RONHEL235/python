f308d3b253bce99525b05ce1cf4bd4b61995e027
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-26 20:37:11 +0200

    Creating a class named person and defining some functions belonging to it.

--- Practice.py
+++ Practice.py
@@ -1182,3 +1182,15 @@ class class_name:
     .
     .
     <statement N>
+
+class Person:
+    def __init__(self, name, sex, profession):
+        self.name = name
+        self.sex = sex
+        self.profession = profession
+
+    def show(self):
+        print("Name: ", self.name, "Sex: ", self.sex, "Profession: ", self.profession)
+
+    def work(self):
+        print(self.name, "working as a ", self.profession)     
