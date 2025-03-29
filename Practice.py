46eb37fa830474c3f42d125f7269eb664d6624b4
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-03-29 17:12:30 +0200

    Understanding Classes even more.

--- Practice.py
+++ Practice.py
@@ -1311,7 +1311,32 @@ Its always when I am doing the mundane that they hit me. I am fighting myself so
 What the frick do I want mara?
 """
 
+#OOP Exercise 5: Define a property that must have the same value for every class instance (object)
 
- 
+#Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+
+#Use the following code for this exercise.
+
+class Vehicle:
+
+    #Class Attribute
+    color = "White"
+
+    #Instance Method
+    def __init__(self, name, max_speed, mileage):
+        
+        #Instance Variables
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
 
 
+school_bus = Bus("Volvo", 180, 20)
+#Color can be called from anywhere or by any class
+print(school_bus.color, school_bus.max_speed)
