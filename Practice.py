349a22ff83c1d4bd8769ccf5952edba444c20cd1
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-04-14 11:32:13 +0200

    There is lots of code to decipher.

--- Practice.py
+++ Practice.py
@@ -1616,4 +1616,47 @@ Seventh and I am done.
 
 #Elite mindset. Body of an Athlete. Soul of God. Character of a Gentleman. Style of 90's Michael Jordan. Healthy Mind. Healthy body. Healthy Soul. 
 
-#I am a God. I am a God. I am a God. I am a God. I am a God. I am a God. I am a God.  
+#I am a God. I am a God. I am a God. I am a God. I am a God. I am a God. I am a God. 
+
+
+#Monday [11:29]
+#14 April 2025
+
+#String manipulation by building a Cipher.
+
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'python'
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
