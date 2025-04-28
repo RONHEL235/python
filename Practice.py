83f669a1593fada48f05a677594523be961c6b9d
Ronee <137155067+RONHEL235@users.noreply.github.com>
2025-04-28 20:28:26 +0200

    .

--- Practice.py
+++ Practice.py
@@ -2150,3 +2150,5 @@ Would you like me to send that too? (No pressure — only if it would help.)
 #"I can't be sobber, U gotta stay high" - gunna
 
 #I have been talking to myself now. You are witnessing greatness in 8k. 
+
+#Baby Keem has not released anything since 2021. Just thought I put it out there. 
